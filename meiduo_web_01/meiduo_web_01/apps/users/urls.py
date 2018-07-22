9a8ff787b0bdd0429d21e0b77459d5a1075d3e13
zfl273 <909736837@qq.com>
2018-07-22 09:56:37 +0800

    登录使用JWT实现

--- meiduo_web_01/meiduo_web_01/apps/users/urls.py
+++ meiduo_web_01/meiduo_web_01/apps/users/urls.py
@@ -1,5 +1,5 @@
 
-
+from rest_framework_jwt.views import obtain_jwt_token
 from django.conf.urls import url
 # from .views import UsernameCountView
 from . import views
@@ -10,4 +10,7 @@ urlpatterns = [
     url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
     # 注册
     url(r'^users/$', views.UserView.as_view()),
+    # jwt登录
+    url(r'^authorizations/$', obtain_jwt_token),
+
 ]
